d087521f9d2d7431022eb63072aa8ece169dbe5d
avidLearnerInProgress <chiragshah9696@gmail.com>
2018-03-05 00:00:37 +0530

    added files

---
+++ 7. color_filtering.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np 
+
+cap = cv2.VideoCapture('http://192.168.0.2:8080/video')
+
+while True:
+	_, frame = cap.read()
+	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	lower_red = np.array([150,150,50])
+	upper_red = np.array([180,255,150])
+
+	mask = cv2.inRange(hsv, lower_red, upper_red)
+	res = cv2.bitwise_and(frame, frame, mask = mask)
+
+	cv2.imshow('frame', frame)
+	cv2.imshow('mask', mask)
+	cv2.imshow('res', res)
+
+	k = cv2.waitKey(5) & 0xFF
+	if k == 27:
+		break
+
+cv2.destroyAllWindows()
+cap.release()
+	#dar_red = np.uint8([[]])
